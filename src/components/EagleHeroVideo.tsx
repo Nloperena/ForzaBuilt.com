6d032a334b8d945822456d368f03b00d86f92a62
Nico <nicholasloperena@gmail.com>
2025-11-12 14:26:19 -0500

    Add top padding to eagle hero video on larger displays (md:pt-12, except 2xl:pt-0)

--- src/components/EagleHeroVideo.tsx
+++ src/components/EagleHeroVideo.tsx
@@ -29,7 +29,7 @@ const EagleHeroVideo: React.FC = () => {
   };
 
   return (
-    <section className="relative h-[60vh] md:h-screen overflow-hidden bg-gradient-to-b from-[#2c476e] to-[#81899f] shadow-2xl">
+    <section className="relative h-[60vh] md:h-screen overflow-hidden bg-gradient-to-b from-[#2c476e] to-[#81899f] shadow-2xl md:pt-12 2xl:pt-0">
       {/* Video Skeleton Loading State */}
       {!isVideoLoaded && (
         <VideoSkeleton />
