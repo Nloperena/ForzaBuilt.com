5dce1206e71f5d949b232e17bd886cf462f82d5b
Nico <nicholasloperena@gmail.com>
2025-12-16 15:46:05 -0500

    Use blue logo on white background in mobile navigation menu

--- src/components/Header/HeaderV2.tsx
+++ src/components/Header/HeaderV2.tsx
@@ -277,7 +277,7 @@ const HoverDropdown: React.FC<{ items: MenuItem[]; widthClass?: string; variant?
             >
               {/* Header */}
               <div className="sticky top-0 bg-white border-b border-gray-200 p-6 z-10 flex items-center justify-between">
-                <Logo className="h-10 w-auto" isWhiteBackground={false} />
+                <Logo className="h-10 w-auto" isWhiteBackground={true} />
                 <button
                   onClick={() => setMobileMenuOpen(false)}
                   className="p-2 hover:bg-gray-100 rounded-full transition-colors"
